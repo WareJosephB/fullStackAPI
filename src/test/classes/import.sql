c653bf22167c5d4ba79c832532758d8315ecee1f
WareJosephB <warejosephb@gmail.com>
2018-10-26 16:03:14 +0100

    end of day

--- src/test/classes/import.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO MOVIE (id, title, genre, rating) VALUES (1, 'Title111', 'Genre111', '111')
-INSERT INTO MOVIE (id, title, genre, rating) VALUES (2, 'Title222', 'Genre222', '222')
-INSERT INTO MOVIE (id, title, genre, rating) VALUES (3, 'Title333', 'Genre333', '333')
